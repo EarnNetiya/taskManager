5f630eca94af191cc465c863600004465ace3efc
EarnNetiya <https://github.com/EarnNetiya/Network_programming>
2025-04-04 19:49:57 +0700

    g

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ const protect = async (req, res, next) => {
     try {
         let token = req.headers.authorization;
 
-        if (token && token.startWith('Bearer')) {
+        if (token && token.startsWith('Bearer')) {
             token = token.split(' ')[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select('-password');
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // auth routes
 router.post("/register", registerUser);  // Register a new user
 router.post("/login", loginUser); // Login a user
-router.get("profile", protect, getUserProfile); // Get user profile
+router.get("/profile", protect, getUserProfile); // Get user profile
 router.put("/profile", protect, updateUserProfile); // Update user profile
 
 module.exports = router;
