c0ad0d0973293fdebd170f3a98ad8c72b2bf41f4
EarnNetiya <https://github.com/EarnNetiya/Network_programming>
2025-04-05 20:51:55 +0700

    ssss

---
+++ backend/controllers/userController.js
@@ -0,0 +1,32 @@
+const Task = require("../model/Task");
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+
+
+// Get all users
+// GET /api/users
+const getUsers = async (req, res) => {
+   try {
+    
+   } catch (error) {
+      res.status(500).json({ message: "Server error", error: error.message });
+   }
+};
+// Get user by ID
+// GET /api/users/:id
+const getUsersByid = async (req, res) => {
+    try {
+    } catch (error) {
+       res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+// Delete user
+// DELETE /api/users/:id
+const deleteUser = async (req,res) => {
+    try {
+    } catch (error) {
+       res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+module.exports = { getUsers, getUsersByid, deleteUser };
---
+++ backend/routes/userRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { adminOnly, protect } = require('../middleware/authMiddleware');
+const { getUsers, getUsersByid, deleteUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+// User Management Routes
+router.get('/', protect, adminOnly, getUsers);
+router.get('/:id', protect, getUsersByid);
+router.delete('/:id', protect, adminOnly, deleteUser);
+
+module.exports = router;
