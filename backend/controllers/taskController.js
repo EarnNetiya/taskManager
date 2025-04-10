be93e18e71c28bd66c2177cd83771fc4c0b9069e
EarnNetiya <https://github.com/EarnNetiya/Network_programming>
2025-04-10 20:22:05 +0700

    new

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,1 +1,103 @@
-// 59.49
+const Task = require('../models/taskModel');
+
+// Get all tasks
+// GET /api/tasks
+const getTasks = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+// Get task by ID
+// GET /api/tasks/:id
+const getTaskById = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+// Create a new task
+// POST /api/tasks
+const createTask = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+// Update a task
+// PUT /api/tasks/:id
+const updateTask = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+// Delete a task
+// DELETE /api/tasks/:id
+const deleteTask = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+// update task status
+// PUT /api/tasks/:id/status
+const updateTaskStatus = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+// Update task checklist
+// PUT /api/tasks/:id/todo
+const updateTaskChecklist = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+// Dashboard data for admin
+// GET /api/tasks/dashboard-data
+const getDashboardData = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+// Dashboard data for user
+// GET /api/tasks/user-dashboard-data
+const getUserDashboardData = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+module.exports = {
+    getTasks,
+    getTaskById,
+    createTask,
+    updateTask,
+    deleteTask,
+    updateTaskStatus,
+    updateTaskChecklist,
+    getDashboardData,
+    getUserDashboardData,
+};
---
+++ backend/routes/taskRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { protect, adminOnly } = require('../middleware/authMiddleware');
+const { getDashboardData, getUserDashboardData, getTasks, getTaskById, createTask, updateTask, deleteTask, updateTaskStatus, updateTaskChecklist } = require('../controllers/taskController');
+
+
+const router = express.Router();
+
+// Task Management Routes
+router.get("dashboard-data", protect, getDashboardData);
+router.get("user-dashboard-data", protect, getUserDashboardData);
+router.get("/", protect, getTasks); // Get all tasks
+router.get("/:id", protect, getTaskById); // Get task by ID
+router.post("/", protect, adminOnly, createTask); // Create a new task
+router.put("/:id", protect, updateTask); // Update a task
+router.delete("/:id", protect, adminOnly, deleteTask); // Delete a task
+router.put("/:id/status", protect, updateTaskStatus); // Update task status
+router.put("/:id/todo", protect, updateTaskChecklist); // Update task checklist
+
+module.exports = router;
